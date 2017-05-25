154e9366bb733cda042d7d1c6ffaa681466fbd9b
DragonSlifer <dragonslifer45@gmail.com>
2017-05-25 18:33:36 +0200

    Mascotas Crear

--- Animalia/src/es/uv/bd/view/MascotaCrear.java
+++ Animalia/src/es/uv/bd/view/MascotaCrear.java
@@ -19,22 +19,19 @@ import javax.swing.*;
  */
 public class MascotaCrear extends JFrame {
     
-    private JTextField idMascota = new JTextField();
-    //private JTextField idCliente = new JTextField();
-    private JComboBox idCliente = new JComboBox();
-    private JTextField nombreMascota = new JTextField();
-    private JTextField tipoAnimal = new JTextField();
-    private JComboBox dia = new JComboBox();
-    private JComboBox mes = new JComboBox();
-    private JComboBox anyo = new JComboBox();
+    private JTextField idCampista = new JTextField();
+    private JComboBox idPatologia = new JComboBox();
+    private JComboBox gravedad = new JComboBox();
+    private JComboBox activa = new JComboBox();
+    private JTextField ind_especiales = new JTextField();
     
     private JTable mascotaTable;
-    private HashMap<String, Integer> listaClientes = new HashMap();
+    private HashMap<String, Integer> listaPatologias = new HashMap();
     private Object [] claves;
         
     public MascotaCrear(JTable mascotaTable) {
             
-        super("Nueva Mascota");
+        super("Nuevo Padecen");
         
         this.mascotaTable = mascotaTable;
         
@@ -62,7 +59,7 @@ public class MascotaCrear extends JFrame {
    
     private JPanel createCabecera() {
         JPanel c = new JPanel();
-        JLabel l = new JLabel("Dar de alta mascota");
+        JLabel l = new JLabel("Dar de alta padecen");
         l.setForeground(Color.BLUE);
         l.setHorizontalAlignment(JLabel.CENTER);
         l.setFont(new Font("Arial",Font.BOLD | Font.ITALIC,22));
@@ -81,78 +78,54 @@ public class MascotaCrear extends JFrame {
         
         try {  
         
-            listaClientes = clienteDao.getTListaClientes();
-            claves = listaClientes.keySet().toArray();
+            listaPatologias = clienteDao.getTListaPatologias();
+            claves = listaPatologias.keySet().toArray();
         
             form.setLayout(new GridLayout(5,2));
             
             
-            idMascota.setFont(textfFont);
-            idMascota.setColumns(4);
-            idCliente.setFont(textfFont);
-            //idCliente.setColumns(4);
-            nombreMascota.setFont(textfFont);
-            nombreMascota.setColumns(30);
-            tipoAnimal.setFont(textfFont);
-            tipoAnimal.setColumns(4);
+            idCampista.setFont(textfFont);
+            idCampista.setColumns(4);
+            idPatologia.setFont(textfFont);
+            gravedad.setFont(textfFont);
+            activa.setFont(textfFont);
             
-            JLabel idMascotaLabel = new JLabel("Id de la mascota:");
-            idMascotaLabel.setFont(labelFont);
-            idMascotaLabel.setHorizontalAlignment(JLabel.RIGHT);
-            form.add(idMascotaLabel);
-            form.add(idMascota);
+            JLabel idCampistaLabel = new JLabel("Id del campista:");
+            idCampistaLabel.setFont(labelFont);
+            idCampistaLabel.setHorizontalAlignment(JLabel.RIGHT);
+            form.add(idCampistaLabel);
+            form.add(idCampista);
             
-            JLabel idClienteLabel = new JLabel("Id del dueño:");
-            idClienteLabel.setFont(labelFont);
-            idClienteLabel.setHorizontalAlignment(JLabel.RIGHT);
-            form.add(idClienteLabel);
+            JLabel idPatologiaLabel = new JLabel("Id de la patologia:");
+            idPatologiaLabel.setFont(labelFont);
+            idPatologiaLabel.setHorizontalAlignment(JLabel.RIGHT);
+            form.add(idPatologiaLabel);
             
-            idCliente.setModel(new DefaultComboBoxModel(claves));
-            //idCliente.setColumns(4);
-            form.add(idCliente);
+            idPatologia.setModel(new DefaultComboBoxModel(claves));
+            
+            form.add(idPatologia);
 
-            JLabel nombreMascotaLabel = new JLabel("Nombre de la mascota:");
+            JLabel nombreMascotaLabel = new JLabel("Gravedad:");
             nombreMascotaLabel.setFont(labelFont);
             nombreMascotaLabel.setHorizontalAlignment(JLabel.RIGHT);
             form.add(nombreMascotaLabel);
-            form.add(nombreMascota);
+            form.add(gravedad);
 
-            JLabel tipoAnimalLabel = new JLabel("Tipo de animal:");
+            JLabel tipoAnimalLabel = new JLabel("Activa:");
             tipoAnimalLabel.setFont(labelFont);
             tipoAnimalLabel.setHorizontalAlignment(JLabel.RIGHT);
             form.add(tipoAnimalLabel);
-            form.add(tipoAnimal);
-
-            JLabel fechaNacimientoLabel = new JLabel("Fecha de nacimiento:");
-            fechaNacimientoLabel.setFont(labelFont);
-            fechaNacimientoLabel.setHorizontalAlignment(JLabel.RIGHT);
-            form.add(fechaNacimientoLabel);
-            
+            form.add(activa);
             
             JPanel fechaPanel = new JPanel();
             fechaPanel.setLayout(new FlowLayout());
-            
-            dia.setModel(new DefaultComboBoxModel(new String[] {"01","02","03","04","05","06","07","08","09","10",
-                    "11","12","13","14","15","16","17","18","19","20",
-                    "21","22","23","24","25","26","27","28","29","30","31"}));
-            mes.setModel(new DefaultComboBoxModel(new String[] {"01","02","03","04","05","06","07","08","09","10",
-                "11","12"}));
-            anyo.setModel(new DefaultComboBoxModel(new String[] {"1990","1991","1992","1993","1994","1995","1996",
-                "1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010",
-                    "2011","2012"}));
-            
-            fechaPanel.add(dia);
-            fechaPanel.add(new JLabel("-"));
-            fechaPanel.add(mes);
-            fechaPanel.add(new JLabel("-"));
-            fechaPanel.add(anyo);
             
             form.add(fechaPanel);
         }
         catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
             JOptionPane.showMessageDialog(
                 null,
-                "Error creando mascota: " + e.getMessage(),
+                "Error creando padecen: " + e.getMessage(),
                 "Atención",
                 JOptionPane.ERROR_MESSAGE);
         }
@@ -166,7 +139,7 @@ public class MascotaCrear extends JFrame {
         JPanel botonPanel = new JPanel();
         
         JButton boton = new JButton("Añadir");
-        boton.setActionCommand("botonMascota");
+        boton.setActionCommand("botonPadecen");
         boton.addActionListener(new ButtonListener());
         botonPanel.add(boton);
         return botonPanel;
@@ -178,26 +151,26 @@ public class MascotaCrear extends JFrame {
             try {
                 String key = event.getActionCommand();
                 switch (key) {
-                    case "botonMascota":
+                    case "botonPadecen":
                         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
                         Padecen mascota = new Padecen();
-                        mascota.setIdMascota(Integer.parseInt(idMascota.getText()));
-                        String seleccion = (String)claves[idCliente.getSelectedIndex()];
-                        int idcli = (int)listaClientes.get(seleccion);
-                        mascota.setIdCliente(idcli);
-                        mascota.setNombreMascota(nombreMascota.getText());
-                        mascota.setTipoAnimal(Integer.parseInt(tipoAnimal.getText()));
-                        String f = (String)dia.getSelectedItem() + "-" + 
-                            (String)mes.getSelectedItem() + "-" +
-                            (String)anyo.getSelectedItem();
-                        mascota.setFechaNacimiento(sdf.parse(f));
-
+                        mascota.setId_campista(Integer.parseInt(idCampista.getText()));
+                        String seleccion = (String)claves[idPatologia.getSelectedIndex()];
+                        int idcli = (int)listaPatologias.get(seleccion);
+                        mascota.setId_patologia(idcli);
+                        mascota.setGravedad((String)gravedad.getSelectedItem());
+                        if("activa".equals((String) activa.getSelectedItem())){
+                            mascota.setActiva(true);
+                        } else {
+                            mascota.setActiva(false);
+                        }
+                        
                         PadecenDAO mascotaDao = new PadecenDAO();
-                        mascotaDao.crearMascota(mascota);
+                        mascotaDao.crearPadecen(mascota);
                         /*
                          * Actualizamos el modelo
                          */
-                        mascotaTable.setModel(mascotaDao.getTablaMascotas());
+                        mascotaTable.setModel(mascotaDao.getTablaPadecen());
                         mascotaTable.updateUI();
                          /*
                          * Cerramos la ventana
@@ -205,14 +178,14 @@ public class MascotaCrear extends JFrame {
                         dispose();
                         break;
                     default:
-                        System.out.println("MascotaCrear: Accion '" + key + "' no reconocida.");
+                        System.out.println("PadecenCrear: Accion '" + key + "' no reconocida.");
                         break;
                 }
             }
-            catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException | ParseException e) {
+            catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
                 JOptionPane.showMessageDialog(
                 null,
-                "Error creando mascota: " + e.getMessage(),
+                "Error creando padecen: " + e.getMessage(),
                 "Atención",
                 JOptionPane.ERROR_MESSAGE);
             }
